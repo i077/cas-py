5b9257040113c7c5c20309711213d16a1ee8834d
dblincoe <dblincoe@gmail.com>
2020-02-11 17:01:31 -0500

    Created History element

---
+++ frontend/src/components/extras/History/History.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Latex from "react-latex";
+import './History.css';
+
+export class History extends React.Component {
+
+    render() {
+
+        return (
+            <div className="history-div">
+
+            </div>
+        );
+    }
+}

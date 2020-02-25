bcea1a796af6c87d25f9a48b8a3ec503c46c408b
Kennan LeJeune <kennan@case.edu>
2020-02-25 13:34:55 -0500

    basic structures fully implemented

--- structures.py
+++ structures.py
@@ -1,5 +1,6 @@
-from abc import ABC, abstractmethod
+import math
 import operator as op
+from abc import ABC, abstractmethod
 
 
 class Function(ABC):
@@ -20,19 +21,28 @@ class Function(ABC):
         return Expression(op.truediv, self, other)
 
     def __pow__(self, power, modulo=None):
-        pass
+        if power > 0:
+            if power == 1:
+                return self
+            else:
+                return self * Expression(op.mul, self, power - 1)
+        elif power < 0:
+            return Number(1) / self.__pow__(power=math.abs(power))
+        else:
+            return Number(1)
 
-    def __lt__(self, other):
-        return Expression(op.lt, self, other)
+    # we can't do this unless we specify function domains -- probably too much complexity?
+    #     def __lt__(self, other):
+    #         return Expression(op.lt, self, other)
 
-    def __gt__(self, other):
-        return Expression(op.gt, self, other)
+    #     def __gt__(self, other):
+    #         return Expression(op.gt, self, other)
 
-    def __ge__(self, other):
-        return Expression(op.ge, self, other)
+    #     def __ge__(self, other):
+    #         return Expression(op.ge, self, other)
 
-    def __le__(self, other):
-        return Expression(op.le, self, other)
+    #     def __le__(self, other):
+    #         return Expression(op.le, self, other)
 
     @abstractmethod
     def __eq__(self, other):
@@ -52,7 +62,8 @@ class Function(ABC):
 
 
 class Expression(Function):
-    def __init__(self, op, left: Function, right: Function, power=1):
+    def __init__(self, op: callable, left: Function, right: Function, power=1):
+        # how should we handle expressions taken to powers?
         self.op = op
         self.left = left
         self.right = right
@@ -62,18 +73,36 @@ class Expression(Function):
         return self.op(self.left.evaluate(x), self.right.evaluate(x))
 
     def __eq__(self, other):
-        return isinstance(other, Expression) and other.left == self.left \
-                                             and other.right == self.right\
-                                             and other.op == self.op
+        return (
+            isinstance(other, Expression)
+            and other.left == self.left
+            and other.right == self.right
+            and other.op == self.op
+        )
 
     def __ne__(self, other):
-        return not isinstance(other, Expression) and other.left == self.left \
-                                             and other.right == self.right\
-                                             and other.op == self.op
+        return not self.__eq__(other)
 
     def derivative(self):
-        pass
+        # if addition or subtraction then we just add or subtract the derivatives
+        if self.op in [op.add, op.sub]:
+            return self.op(self.left.derivative(), self.right.derivative())
+        # product rule
+        elif self.op == op.mul:
+            return (
+                self.left.derivative() * self.right
+                + self.left * self.right.derivative()
+            )
+        # quotient rule
+        elif self.op == op.truediv:
+            return (
+                self.left.derivative() * self.right
+                - self.right.derivative() * self.left
+            ) / (self.right ** 2)
+        else:
+            raise ValueError
 
+    # TODO: how tf do we do this
     def integral(self):
         pass
 
@@ -89,13 +118,13 @@ class Variable(Function):
         return isinstance(f, Variable) and f.name == self.name
 
     def __ne__(self, other):
-        return not isinstance(other, Variable) and other.name == self.name
+        return not (isinstance(other, Variable) and other.name == self.name)
 
     def derivative(self):
         return 1
 
     def integral(self):
-        pass
+        return Polynomial(coeff=1, var=self, power=2) / 2
 
     def __repr__(self) -> str:
         return self.name
@@ -107,15 +136,55 @@ class Polynomial(Function):
         self.var = var
         self.power = power
 
+    def __add__(self, other):
+        if (
+            isinstance(other, Polynomial)
+            and other.var == self.var
+            and other.power == self.power
+        ):
+            return Polynomial(self.coeff + other.coeff, self.var, self.power)
+        else:
+            return super().__add__(other)
+
+    def __sub__(self, other):
+        if (
+            isinstance(other, Polynomial)
+            and other.var == self.var
+            and other.power == self.power
+        ):
+            return Polynomial(self.coeff - other.coeff, self.var, self.power)
+        else:
+            return super().__sub__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial) and other.var == self.var:
+            return Polynomial(
+                self.coeff * other.coeff, self.var, other.power + self.power
+            )
+        else:
+            return super().__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Polynomial) and other.var == self.var:
+            return (
+                Polynomial(self.coeff, self.var, self.power - other.power) / other.coeff
+            )
+        else:
+            return super().__truediv__(other)
+
     def evaluate(self, x):
         val = x.value if isinstance(x, Number) else x
         return self.coeff * (val ** self.power)
 
     def __eq__(self, other):
-        pass
+        return (
+            self.coeff == other.coeff
+            and self.var == other.var
+            and self.power == other.power
+        )
 
     def __ne__(self, other):
-        pass
+        return not self.__eq__(other)
 
     def derivative(self):
         return Polynomial(
@@ -123,12 +192,27 @@ class Polynomial(Function):
         )
 
     def integral(self):
-        pass
+        return Polynomial(coeff=self.coeff, var=self.var, power=self.power + 1) / (
+            self.power + 1
+        )
 
 
 class Number(Function):
     def __init__(self, value):
-        self.value = value
+        if isinstance(value, Number):
+            self.value = value.value
+        else:
+            self.value = value
+
+    def __add__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+        else:
+            super().__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value)
 
     def evaluate(self, x=None):
         return self.value
@@ -141,27 +225,3 @@ class Number(Function):
 
     def __repr__(self):
         return self.value
-
-
-class Fraction(Function):
-    def __init__(self, numerator: Function, denominator: Function):
-        self.numerator = numerator
-        self.denominator = denominator
-
-    def evaluate(self, x):
-        pass
-
-    def __eq__(self, other):
-        pass
-
-    def __ne__(self, other):
-        pass
-
-    def derivative(self):
-        return (
-            self.numerator.derivative() * self.denominator
-            - self.denominator.derivative() * self.numerator
-        ) / (self.denominator * self.denominator)
-
-    def integral(self):
-        pass

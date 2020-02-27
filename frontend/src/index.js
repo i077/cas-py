7d326d82eae958c18331adef97d8037fe2ee91f3
dblincoe <dblincoe@gmail.com>
2020-02-27 13:39:47 -0500

    Scrapped redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-import { Provider } from 'react-redux';
-import store from "./redux/store";
-
 
 const root = document.getElementById('root');
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider >,
-    root);
+ReactDOM.render(<App />, root);

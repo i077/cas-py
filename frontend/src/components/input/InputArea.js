f1efc2d377dfe43c83120a8a07d631a0eebc582f
dblincoe <dblincoe@gmail.com>
2020-02-27 18:33:01 -0500

    Updated input numbers

--- frontend/src/components/input/InputArea.js
+++ frontend/src/components/input/InputArea.js
@@ -8,7 +8,11 @@ export class InputArea extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { input: "", previousSelect: props.selectedText };
+        this.state = {
+            input: "",
+            replacedInput: "",
+            previousSelect: props.selectedText
+        };
 
         this.textAreaChange = this.textAreaChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,8 +33,32 @@ export class InputArea extends React.Component {
         }
     }
 
+    getReplacedInput(input) {
+        var re = /[$]\d+/g, replace = re.exec(input);
+
+        while (replace !== null) {
+            const replNum = parseInt(replace[0].substr(1, replace[0].length - 1));
+            const foundCalc = this.props.history.find((calc) => calc.id === replNum);
+
+            if (foundCalc === undefined) {
+                input = input.replace(replace[0], '\\left( \\text{Reference Not Found} \\right)');
+            } else if (foundCalc.output === "") {
+                input = input.replace(replace[0], '\\left( \\text{Reference has No Output} \\right)');
+            } else {
+                input = input.replace(replace[0], '\\left(' + foundCalc.output + '\\right)');
+            }
+
+            replace = re.exec(input);
+        }
+        return input;
+    }
+
     textAreaChange(e) {
-        this.setState({ input: e.target.value });
+        const val = e.target.value;
+        this.setState({
+            input: val,
+            replacedInput: this.getReplacedInput(val)
+        });
     }
 
     handleSubmit() {
@@ -46,7 +74,7 @@ export class InputArea extends React.Component {
                         <Form.Label>Enter Latex:</Form.Label>
                         <Form.Control as="textarea" rows="6" value={this.state.input} onChange={this.textAreaChange} onKeyUp={this.handleKeyPress} />
 
-                        <LiveInput input={this.state.input} />
+                        <LiveInput input={this.state.replacedInput} />
                     </Form.Group>
                     <Button onClick={this.handleSubmit}>Submit Latex</Button>
                 </Form>

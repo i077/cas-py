17c038c7f1180238887bec47736f6f3545486b4a
Kennan LeJeune <kennan@case.edu>
2020-04-14 07:43:07 -0500

    plz lint your code thx

---
+++ frontend/build/precache-manifest.7f381ecfe55da8f150bb863f056fc358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587b6bc965f1203e029f9baeabefbd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a6ef205d90d445948d",
+    "url": "/static/css/main.88d217c9.chunk.css"
+  },
+  {
+    "revision": "569cf92bfc8b652409a9",
+    "url": "/static/js/2.a516d4a5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a516d4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a6ef205d90d445948d",
+    "url": "/static/js/main.2d46cbbd.chunk.js"
+  },
+  {
+    "revision": "c1c4ee103de11e777c74",
+    "url": "/static/js/runtime-main.28264dbc.js"
+  }
+]);

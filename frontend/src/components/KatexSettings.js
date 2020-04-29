e486e7d4b793fb2ae4c8a8940edbedf7b1a32049
dblincoe <dblincoe@gmail.com>
2020-04-28 20:33:00 -0400

    Updated Katex Settings for rref and neq

--- frontend/src/components/KatexSettings.js
+++ frontend/src/components/KatexSettings.js
@@ -2,6 +2,8 @@ export const KatexSettings = {
     macros: {
         '\\dd': '\\mathrm{d}',
         '\\dv': '\\frac{\\text{d} #1}{\\text{d} #2}',
-        '\\pdv': '\\frac{\\partial #1}{\\partial #2}'
+        '\\pdv': '\\frac{\\partial #1}{\\partial #2}',
+        '\\rref': '\\text{rref}',
+        '\\neq': '\\cancel{=}'
     }
 }
